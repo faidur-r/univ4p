d361d415ca54ad318cdf621e3025897354a6c717
faidur-r <faidurfti@gmail.com>
2021-07-15 18:42:36 +0800

    Upload file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>APP UNIV</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
